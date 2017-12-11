cce84356d492d694e7a1ee78f216215281fee8a5
teodoratanasov <t.atanasov1516@gmail.com>
2017-12-11 16:57:07 +0200

    Update update.php

--- admin/update.php
+++ admin/update.php
@@ -11,7 +11,7 @@ $price = $_GET['sprice'];
 $quantity = $_GET['squantity'];
 
 
-//create query
+//create query to order stock 
 
 $query = "UPDATE stock SET item_name='$name', item_price='$price', item_quantity='$quantity' WHERE item_id='$id'";
 
